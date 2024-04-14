a675c2bbd9326297ad3106031476182cd9259591
chkan <chkan.dv@gmail.com>
2024-04-14 15:01:45 +0300

    008 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,19 +2,44 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val KEY_HIDE_STATE = "myState"
+    }
+
+    private var isHided: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.changeButton)
+        val textView = findViewById<TextView>(R.id.titleTextView)
+        val btnChange = findViewById<Button>(R.id.changeButton)
+        val btnHide = findViewById<Button>(R.id.hideButton)
+        textView.isVisible = !isHided
 
-        button.setOnClickListener {
-            findViewById<TextView>(R.id.titleTextView).text = "I am an Android Developer!"
+        btnChange.setOnClickListener {
+            textView.text = "I am an Android Developer!"
+        }
+        btnHide.setOnClickListener {
+            textView.isVisible = false
+            isHided = true
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(KEY_HIDE_STATE, isHided)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        isHided = savedInstanceState.getBoolean(KEY_HIDE_STATE)
+    }
+
 }
