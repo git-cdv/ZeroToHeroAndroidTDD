1f1db4b12921e3e14a32dfb43c8429782cd94b4b
chkan <chkan.dv@gmail.com>
2024-04-18 09:52:22 +0300

    009 solved with OOP

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,9 +2,12 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.isVisible
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,16 +15,21 @@ class MainActivity : AppCompatActivity() {
         private const val KEY_HIDE_STATE = "myState"
     }
 
+    private lateinit var rootLayout : LinearLayout
+    private lateinit var textView : TextView
+
     private var isHided: Boolean = false
+    private var state: State = State.Initial
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.titleTextView)
+        rootLayout = findViewById(R.id.rootLayout)
+        textView = findViewById(R.id.titleTextView)
         val btnChange = findViewById<Button>(R.id.changeButton)
         val btnHide = findViewById<Button>(R.id.hideButton)
-        textView.isVisible = !isHided
+        val btnRemove = findViewById<Button>(R.id.removeButton)
 
         btnChange.setOnClickListener {
             textView.text = "I am an Android Developer!"
@@ -30,16 +38,36 @@ class MainActivity : AppCompatActivity() {
             textView.isVisible = false
             isHided = true
         }
+        btnRemove.setOnClickListener {
+            state = State.Removed
+            state.apply(rootLayout,textView)
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putBoolean(KEY_HIDE_STATE, isHided)
+        outState.putSerializable(KEY_HIDE_STATE, state)
         super.onSaveInstanceState(outState)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        isHided = savedInstanceState.getBoolean(KEY_HIDE_STATE)
+        state = savedInstanceState.getSerializable(KEY_HIDE_STATE) as State
+        state.apply(rootLayout,textView)
     }
 
+}
+
+interface State : Serializable {
+
+    fun apply (linearLayout : LinearLayout, textView : TextView)
+
+    object Initial : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView)  = Unit
+    }
+
+    object Removed : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView){
+            linearLayout.removeView(textView)
+        }
+    }
 }
